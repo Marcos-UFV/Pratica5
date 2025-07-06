19e0c4547cc4e1d8bc656be1398c1cbb6009d0e8
Marcos <aurelio.marcos@ufv.br>
2025-07-06 16:57:17 -0300

    update: transfere responsabilidade de retornar as entidades para o BD

--- app/src/main/java/br/ufv/dpi/inf311/pratica5/BancoDados.java
+++ app/src/main/java/br/ufv/dpi/inf311/pratica5/BancoDados.java
@@ -6,6 +6,13 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
+import br.ufv.dpi.inf311.pratica5.model.Categoria;
+import br.ufv.dpi.inf311.pratica5.model.Local;
+
 public class BancoDados {
     protected SQLiteDatabase db;
     private final String NOME_BANCO = "pratica5_bd";
@@ -53,7 +60,39 @@ public class BancoDados {
         Log.i("BANCO_DADOS", "Realizou uma busca e retornou [" + c.getCount() + "] registros.");
         return c;
     }
-
+    public List<Local> getTodosLocaisVisitados(){
+        List<Local> locaisVisitados = new ArrayList<>();
+        Cursor c = INSTANCE.buscar("Checkin",new String[]{"Local","qtdVisitas","cat","latitude","longitude"},"","");
+        while(c.moveToNext()){
+            int local = c.getColumnIndex("Local");
+            int qtd = c.getColumnIndex("qtdVisitas");
+            int latitude = c.getColumnIndex("latitude");
+            int longitude = c.getColumnIndex("longitude");
+            Local l = new Local(c.getString(local), c.getInt(qtd), c.getString(latitude), c.getString(longitude),null);
+            locaisVisitados.add(l);
+            String msg = String.format("Local: "+l);
+            Log.i("LOCATION",msg);
+        }
+        Log.i("LOCATION","Nº de loacais visitados: "+locaisVisitados.size());
+        return locaisVisitados;
+    }
+    public List<Local> getTodosLocaisVisitadosComCategorias(){
+        Cursor c = INSTANCE.buscar("Checkin c, Categoria ca", new String[]{"c.Local local", "c.qtdVisitas visitas", "c.latitude latitude", "c.longitude longitude", "ca.idCategoria idCategoria", "ca.nome nomeCategoria"}, "c.cat = ca.idCategoria", null);
+        List<Local> locaisVisitados = new ArrayList<>();
+        while(c.moveToNext()){
+            int local = c.getColumnIndex("local");
+            int qtd = c.getColumnIndex("visitas");
+            int latitude = c.getColumnIndex("latitude");
+            int longitude = c.getColumnIndex("longitude");
+            int id = c.getColumnIndex("idCategoria");
+            int nome = c.getColumnIndex("nomeCategoria");
+            Categoria categoria = new Categoria(c.getInt(id), c.getString(nome));
+            Local l = new Local(c.getString(local), c.getInt(qtd), c.getString(latitude), c.getString(longitude), categoria);
+            locaisVisitados.add(l);
+            Log.i("MAP","Categoria buscada - ID: "+c.getInt(id)+"Nome: "+c.getString(nome));
+        }
+        return locaisVisitados;
+    }
     public long inserir(String tabela, ContentValues valores) {
         long id = db.insert(tabela, null, valores);
         Log.i("BANCO_DADOS", "Cadastrou registro com o id [" + id + "]");
